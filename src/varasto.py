159d48799ecaa912a0a5847cf83239d20c4c5009
Eeli Häkkinen <eeli.hakkinen00@gmail.com>
2025-11-03 20:06:52 +0200

    Lisätty 2kpl virhettä

--- src/varasto.py
+++ src/varasto.py
@@ -30,12 +30,12 @@ class Varasto:
 
     def ota_varastosta(self, maara):
         if maara < 0:
-            return 0.0
+            return -1 #Virhe 0.0
         if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
-            return kaikki_mita_voidaan
+            return kaikki_mita_voidaan -1 # Virhe
 
         self.saldo = self.saldo - maara
 
